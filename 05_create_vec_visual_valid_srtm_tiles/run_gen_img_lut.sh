7bb153b44f15173e25a65569bbd5d87f7f8a5e3a
Pete Bunting <petebunting@mac.com>
2021-06-08 23:29:59 +0100

    Add script to create LUT for SRTM tiles.

---
+++ 05_create_vec_visual_valid_srtm_tiles/run_gen_img_lut.sh
@@ -0,0 +1,1 @@
+singularity exec --bind /scratch/a.hek4:/scratch/a.hek4 --bind /home/a.hek4:/home/a.hek4 /scratch/a.hek4/swimage/au-eoed-dev.sif python rsgisbuildimglut.py -i "/scratch/a.hek4/srtm/srtm_overlap_tiles/*.tif" -o ./srtm_tiles_overlap_img_lut.gpkg --veclyr srtm_overlap_lut --vecformat GPKG --overwrite
